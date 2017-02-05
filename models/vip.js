7b539f7553d9a10257bcf71581c7dc62d65849c6
MichelCha <michel.chastagner@yahoo.fr>
2017-02-05 19:25:59 +0100

    modifié :         models/vip.js

--- models/vip.js
+++ models/vip.js
@@ -4,7 +4,7 @@ var db = require('../configDb');
 module.exports.test = function(callback) {
     db.getConnection(function(err, connexion) {
         if (!err) {
-            var sql = "SELECT count(*) as NB FROM vip ;";
+            var sql = "SELECT COUNT(*) AS NB FROM vip ;";
               // console.log(sql);
             connexion.query(sql, callback);
             connexion.release();
